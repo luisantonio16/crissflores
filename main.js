e38cb1e7a591cfc3735feecdbd1e9d5c8800a485
¨luisantonio16¨ <luis16.16m@gmail.com>
2024-09-20 21:11:29 -0400

    bien

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 onload = () => {
     document.body.classList.remove("container");
   };
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const titles = ['para Jahaziel', 'Una mujer con una risa encantadora'];
+    let index = 0;
+  
+    function changeTitle() {
+      document.getElementById('nombre').textContent = titles[index];
+      index = (index + 1) % titles.length;
+    }
+  
+    setInterval(changeTitle, 4000);
+  });
+  
