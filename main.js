b5519f36baf08da90c86ded050971660a3c0b1a4
Luis Antonio Mercedes Melenciano <70826577+luisantonio16@users.noreply.github.com>
2025-08-30 22:52:12 -0400

    update

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ onload = () => {
 
 
   document.addEventListener('DOMContentLoaded', function() {
-    const titles = ['para una mujer linda','y' ,'Maravillosa'];
+    const titles = ['Para una mujer linda y complicada','pero cautivadora y peculiar a la vez ' ,'espero que me des la oportunidad de cocnocerte mas', 'y ver que tan dificil y encantadora puedes ser.'];
     let index = 0;
   
     function changeTitle() {
